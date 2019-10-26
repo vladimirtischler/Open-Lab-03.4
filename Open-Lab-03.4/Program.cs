c97f03a3557591353c0e698b9b4bf673998a449c
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-26 18:45:45 +0200

    open lab

---
+++ Open-Lab-03.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Open_Lab_03._4
+{
+    class Program
+    {
+        public static string EvenOrOdd(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return "even";
+            }
+            else
+            {
+                return "odd";
+            }
+        }
+        static void Main(string[] args)
+        {
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(EvenOrOdd(b));
+        }
+    }
+}
